b9ebe91a05b3a4446e466b81659e53f70e88f93f
Matt Kaniaris <mkaniaris@gmail.com>
2012-08-15 17:46:19 -0700

    more working tests.  now in different suites

--- tests/test_trace.js
+++ tests/test_trace.js
@@ -9,7 +9,6 @@ var trace = require('../trace');
 var MAX_ID = Math.pow(2, 31) -1;
 
 ass.isNum = function(num, message){
-  console.log(this);
   ass.equal(isNaN(num), false,
     util.format("%s is not number like", num));
 };
@@ -28,107 +27,108 @@ var mockTracer = function(name, id, endpoint){
 };
 
 module.exports = {
-  test_new_trace: function(test){
-    var t = new trace.Trace('test_trace');
-    test.notEqual(t.traceId, undefined);
-    test.isNum(t.traceId);
-    test.ok(t.traceId < MAX_ID);
+  traceTests: {
+    setUp: function(cb){
+      tracers.setTracers([]);
+      cb();
+    },
+    tearDown: function(cb){
+      tracers.setTracers([]);
+      cb();    
+    },
+    test_new_trace: function(test){
+      var t = new trace.Trace('test_trace');
+      test.notEqual(t.traceId, undefined);
+      test.isNum(t.traceId);
+      test.ok(t.traceId < MAX_ID);
 
-    test.notEqual(t.spanId, undefined);
-    test.isNum(t.spanId);
-    test.ok(t.spanId < MAX_ID);
+      test.notEqual(t.spanId, undefined);
+      test.isNum(t.spanId);
+      test.ok(t.spanId < MAX_ID);
 
-    test.equal(t.parentSpanId, undefined);
-    test.done();
-  },
+      test.equal(t.parentSpanId, undefined);
+      test.done();
+    },
+    test_trace_child: function(test){
+      var t = new trace.Trace('test_trace', {traceId: 1, spanId: 1});
+      var c = t.child('child_test_trace');
+      test.equal(c.traceId, 1);
+      test.equal(c.parentSpanId, 1);
+      test.done();
+    },
     test_record_invokes_tracer: function(test){
-    var tracer, t, a;
-    tracer = new mockTracer();
-    t = new trace.Trace('test_trace', {
-      traceId: 1, spanId: 1, tracers: [tracer]
-    });
-    a = trace.Annotation.clientSend(0);
-    t.record(a);
-    test.equal(1,1);
-    test.deepEqual(tracer._calls.record[0], [t,a]);
-    test.done();
+      var tracer, t, a;
+      tracer = new mockTracer();
+      t = new trace.Trace('test_trace', {
+        traceId: 1, spanId: 1, tracers: [tracer]
+      });
+      a = trace.Annotation.clientSend(0);
+      t.record(a);
+      test.deepEqual(tracer._calls.record[0], [t,a]);
+      test.done();
+    },
+    test_record_sets_annotation_endpoint: function(test){
+      var tracer, e, t, a;
+      tracer = new mockTracer();
+      e = new trace.Endpoint('127.0.0.1', 8080, 'web');
+      t = new trace.Trace('test_trace',
+        {trace_id: 1, span_id: 1, tracers: [tracer]});
+      t.setEndpoint(e);
+      a = new trace.Annotation.clientSend(1);
+      t.record(a);
+      test.deepEqual(tracer._calls.record[0], [t,a]);
+      test.deepEqual(a.endpoint, e);
+      test.done();
+    }
   },
-  test_trace_child: function(test){
-    var t = new trace.Trace('test_trace', {traceId: 1, spanId: 1});
-    var c = t.child('child_test_trace');
-    test.equal(c.traceId, 1);
-    test.equal(c.parentSpanId, 1);
-    test.done();
-  },
-  setUp: function(cb){
-    tracers.setTracers([]);
-    cb();
-  },
-  tearDown: function(cb){
-    tracers.setTracers([]);
-    cb();    
+  annotationTests: {
+    setUp: function(cb){
+      var self = this;
+      self.origDate = Date.now;
+      Date.now = function() {
+        return 1000;
+      };
+      cb();
+    },
+    tearDown: function(cb){
+      var self = this;
+      Date.now = self.origDate;
+      cb();    
+    },
+    test_timestamp: function(test) {
+      var a = trace.Annotation.timestamp('test');
+      test.equal(a.name, 'test');
+      test.equal(a.value, 1000000);
+      test.equal(a.annotationType, 'timestamp');
+      test.done();
+    },
+    test_client_send: function(test) {
+      var a = trace.Annotation.clientSend();
+      test.equal(a.value, 1000000);
+      test.equal(a.name, 'cs');
+      test.equal(a.annotationType, 'timestamp');
+      test.done();
+    },
+    test_client_recv: function(test) {
+      var a = trace.Annotation.clientRecv();
+      test.equal(a.value, 1000000);
+      test.equal(a.name, 'cr');
+      test.equal(a.annotationType, 'timestamp');
+      test.done();
+    },
+    test_server_send: function(test) {
+      var a = trace.Annotation.serverSend();
+      test.equal(a.value, 1000000);
+      test.equal(a.name, 'ss');
+      test.equal(a.annotationType, 'timestamp');
+      test.done();
+    },
+    test_server_recv: function(test) {
+      var a = trace.Annotation.serverRecv();
+      test.equal(a.value, 1000000);
+      test.equal(a.name, 'sr');
+      test.equal(a.annotationType, 'timestamp');
+      test.done();
+    }
   }
-
 };
-
-//     def 
-//         
-
-//         
-//         
-//         
-
-//         
-
-//     def test_record_sets_annotation_endpoint(self):
-//         tracer = mock.Mock()
-//         web_endpoint = Endpoint('127.0.0.1', 8080, 'web')
-
-//         t = Trace('test_trace', trace_id=1, span_id=1, tracers=[tracer])
-//         t.set_endpoint(web_endpoint)
-//         annotation = Annotation.client_send(timestamp=1)
-//         t.record(annotation)
-
-//         tracer.record.test_called_with(t, annotation)
-
-//         self.testEqual(annotation.endpoint, web_endpoint)
-
-
-// class AnnotationTests(TestCase):
-//     def setUp(self):
-//         self.time_patcher = mock.patch('tryfer.trace.time.time')
-//         self.time = self.time_patcher.start()
-//         self.time.return_value = 1
-
-//     def tearDown(self):
-//         self.time_patcher.stop()
-
-//     def test_timestamp(self):
-//         a = Annotation.timestamp('test')
-//         self.testEqual(a.value, 1000000)
-//         self.testEqual(a.name, 'test')
-//         self.testEqual(a.annotation_type, 'timestamp')
-
-//     def test_client_send(self):
-//         a = Annotation.client_send()
-//         self.testEqual(a.value, 1000000)
-//         self.testEqual(a.name, 'cs')
-//         self.testEqual(a.annotation_type, 'timestamp')
-
-//     def test_cleint_recv(self):
-//         a = Annotation.client_recv()
-//         self.testEqual(a.value, 1000000)
-//         self.testEqual(a.name, 'cr')
-//         self.testEqual(a.annotation_type, 'timestamp')
-
-//     def test_server_send(self):
-//         a = Annotation.server_send()
-//         self.testEqual(a.value, 1000000)
-//         self.testEqual(a.name, 'ss')
-//         self.testEqual(a.annotation_type, 'timestamp')
-
-//     def test_server_recv(self):
-//         a = Annotation.server_recv()
-//         self.testEqual(a.value, 1000000)
-//         self.testEqual(a.name, 'sr')
-//         self.testEqual(a.annotation_type, 'timestamp')
