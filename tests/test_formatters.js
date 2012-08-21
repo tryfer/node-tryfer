6a792d1a11146e67ea7991345246bf5c94630acf
cyli <ying.li@rackspace.com>
2012-08-21 11:01:05 -0700

    Finish refactoring tests and adding test for zipkin formatter. ipv4 formatting broken

--- tests/test_formatters.js
+++ tests/test_formatters.js
@@ -8,32 +8,64 @@ var formatters = require('..').formatters;
 var trace = require('..').trace;
 var zipkinCore_types = require('../lib/_thrift/zipkinCore/zipkinCore_types');
 
-var traces_and_annotations = {
+// The test cases for the formatters - basic formatting of trace with
+// timestamp annotation and other annotation, one with a parentSpanId in the
+// trace, and one with an endpoint in one of the annotations
+var testcases = {
   basic_trace_and_annotations: {
     trace: new trace.Trace('test', {spanId: 1, traceId:5}),
     annotations: [new trace.Annotation.timestamp('name1', 1),
                   new trace.Annotation.string('name2', '2')]
+  },
+  trace_with_parentSpanId: {
+    trace: new trace.Trace('test', {parentSpanId:1, spanId: 2, traceId:5}),
+    annotations: []
+  },
+  trace_with_annotation_with_endpoint: {
+    trace: new trace.Trace('test', {spanId: 1, traceId:5}),
+    annotations: [
+      new trace.Annotation(
+        'name1', 1, 'timestamp', (new trace.Endpoint('1.1.1.1', 5, 'service'))
+      )]
   }
 };
 
-// Helper function to convert a base64 string back into a span, and then to
-// run tests on it
-var testBase64Thrift = function (base64str, test_callback) {
-  var b = new Buffer(base64str, "base64");
-  var trans_receiver = ttransport.TBufferedTransport.receiver(function(trans) {
-    var prot = new tprotocol.TBinaryProtocol(trans);
-    var span = new zipkinCore_types.Span();
-    span.read(prot);
-    test_callback(null, span);
-  });
-  trans_receiver(b);
+// Helper function to test formatting for Reskin - takes a test object,
+// one of the test cases from above, and the expected object represented
+// by the formatted json string
+var testRestkinFormatter = function (test, testcase, expected) {
+  formatters.formatForRestkin(testcase.trace, testcase.annotations,
+    function(formatError, jsonStr) {
+      test.equal(formatError, null);
+      test.deepEqual(JSON.parse(jsonStr), expected);
+      test.done();
+    });
+};
+
+// Helper function to test formatting for Zipkin - takes a test object,
+// one of the test cases from above, and the expected zipkinCore_types object
+// represented base64 encoded string
+var testZipkinFormatter = function (test, testcase, expected) {
+  formatters.formatForZipkin(testcase.trace, testcase.annotations,
+    function(formatError, base64str) {
+      var b = new Buffer(base64str, "base64");
+      var trans_receiver = ttransport.TBufferedTransport.receiver(function(trans) {
+        var prot = new tprotocol.TBinaryProtocol(trans);
+        var span = new zipkinCore_types.Span();
+        span.read(prot);
+        test.deepEqual(span, expected);
+        test.done();
+      });
+
+      test.equal(formatError, null);
+      trans_receiver(b);
+    });
 };
 
 module.exports = {
   restkinFormatterTests: {
     test_basic_trace_and_annotations: function(test){
-      var testcase = traces_and_annotations.basic_trace_and_annotations;
-      var expected = {
+      testRestkinFormatter(test, testcases.basic_trace_and_annotations, {
         trace_id: '5',
         span_id: '1',
         name: 'test',
@@ -49,85 +81,86 @@ module.exports = {
             type: 'string'
           }
         ]
-      };
-      formatters.formatForRestkin(testcase.trace, testcase.annotations,
-        function(error, value) {
-          test.equal(error, null);
-          test.deepEqual(JSON.parse(value), expected);
-          test.done();
-        });
+      });
     },
     test_trace_with_parentSpanId: function(test){
-      var t, a, expected;
-      t = new trace.Trace('test', {parentSpanId:1, spanId: 2, traceId:5});
-      a = [];
-      expected = {
+      testRestkinFormatter(test, testcases.trace_with_parentSpanId, {
         trace_id: '5',
         parent_span_id: '1',
         span_id: '2',
         name: 'test',
         annotations: []
-      };
-      formatters.formatForRestkin(t, a, function(error, value) {
-        test.equal(error, null);
-        test.deepEqual(JSON.parse(value), expected);
-        test.done();
       });
     },
     test_trace_with_annotation_with_endpoint: function(test) {
-      var t, a, expected;
-      t = new trace.Trace('test', {spanId: 1, traceId:5});
-      a = [new trace.Annotation('name1', 1, 'test',
-                                new trace.Endpoint('1.1.1.1', 5, 'service'))];
-      expected = {
-        trace_id: '5',
-        span_id: '1',
-        name: 'test',
-        annotations: [
-          {
-            key: 'name1',
-            value: 1,
-            type: 'test',
-            host: {
-              ipv4: '1.1.1.1',
-              port: 5,
-              service_name: 'service'
-            }
-          }
-        ]
-      };
-      formatters.formatForRestkin(t, a, function(error, value) {
-        test.equal(error, null);
-        test.deepEqual(JSON.parse(value), expected);
-        test.done();
-      });
+      testRestkinFormatter(
+        test, testcases.trace_with_annotation_with_endpoint, {
+            trace_id: '5',
+            span_id: '1',
+            name: 'test',
+            annotations: [
+              {
+                key: 'name1',
+                value: 1,
+                type: 'timestamp',
+                host: {
+                  ipv4: '1.1.1.1',
+                  port: 5,
+                  service_name: 'service'
+                }
+              }
+            ]
+          });
     }
   },
   zipkinFormatterTests: {
     test_basic_trace_and_annotations: function(test){
-      var testcase = traces_and_annotations.basic_trace_and_annotations;
-      var expected = new zipkinCore_types.Span({
-        trace_id: 5,
-        id: 1,
-        name: 'test',
-        annotations: [ new zipkinCore_types.Annotation({
-          timestamp: 1,
-          value: 'name1'
-        })],
-        binary_annotations: [ new zipkinCore_types.BinaryAnnotation({
-          value: '2',
-          key: 'name2',
-          annotation_type: zipkinCore_types.AnnotationType.STRING
-        })]
-      });
-      formatters.formatForZipkin(testcase.trace, testcase.annotations,
-        function(formatError, base64) {
-          test.equal(formatError, null);
-          testBase64Thrift(base64, function(parsingError, span) {
-            test.deepEqual(span, expected);
-            test.done();
-          });
-        });
+      testZipkinFormatter(
+        test, testcases.basic_trace_and_annotations,
+        new zipkinCore_types.Span({
+          trace_id: 5,
+          id: 1,
+          name: 'test',
+          annotations: [ new zipkinCore_types.Annotation({
+            timestamp: 1,
+            value: 'name1'
+          })],
+          binary_annotations: [ new zipkinCore_types.BinaryAnnotation({
+            value: '2',
+            key: 'name2',
+            annotation_type: zipkinCore_types.AnnotationType.STRING
+          })]
+        }));
+    },
+    test_trace_with_parentSpanId: function(test){
+      testZipkinFormatter(
+        test, testcases.trace_with_parentSpanId,
+        new zipkinCore_types.Span({
+          trace_id: 5,
+          parent_id: 1,
+          id: 2,
+          name: 'test',
+          annotations: [],
+          binary_annotations: []
+        }));
+    },
+    test_trace_with_annotation_with_endpoint: function(test) {
+      testZipkinFormatter(
+        test, testcases.trace_with_annotation_with_endpoint,
+        new zipkinCore_types.Span({
+            trace_id: '5',
+            span_id: '1',
+            name: 'test',
+            annotations: [ new zipkinCore_types.Annotation({
+              timestamp: 1,
+              value: 'name1',
+              host: new zipkinCore_types.Endpoint({
+                ipv4: '1.1.1.1',
+                port: 5,
+                service_name: 'service'
+              })
+            })]
+          }));
     }
   }
 };
