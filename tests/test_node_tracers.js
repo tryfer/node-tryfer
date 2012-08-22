13f7d3960986a84a4083b80b36c288f5c8e87599
cyli <ying.li@rackspace.com>
2012-08-22 10:37:39 -0700

    test restkin postdata to make sure json is parsable

--- tests/test_node_tracers.js
+++ tests/test_node_tracers.js
@@ -1,6 +1,7 @@
 var trace = require('..').trace;
 var node_tracers = require('..').node_tracers;
 var http = require('http');
+var util = require('util');
 
 var mockKeystoneClient = {
   getTenantIdAndToken: function(cb) {
@@ -14,13 +15,38 @@ module.exports = {
       var self = this;
       var t = new trace.Trace('clientRecv');
       var a = trace.Annotation.clientRecv(2);
+
       self.server = http.createServer(function(request, response) {
         test.equal(request.headers['x-auth-token'], '1');
         test.equal(request.headers['x-tenant-id'], '3');
         test.notEqual(request.headers['content-length'], '0');
-        response.end();
-        self.server.close();
-        test.done();
+        self.postData = new Buffer(
+          parseInt(request.headers['content-length'], 10));
+        self.postLength = 0;
+
+        // -- get the post data and assert that it is parsable as JSON, and
+        // is a non-zero length array
+        request.on('data', function(chunk){
+          if (Buffer.isBuffer(chunk)) {
+            chunk.copy(self.postData, self.postLength);
+          } else {
+            self.postData.write(chunk);
+          }
+          self.postLength += chunk.length;
+        });
+
+        request.on('end', function() {
+          var jsonObj;
+
+          jsonObj = JSON.parse(self.postData.toString());
+          test.ok(util.isArray(jsonObj));
+          test.ok(jsonObj.length >= 1);
+
+          response.end();
+          self.server.close();
+          test.done();
+        });
+
       }).listen(22222, 'localhost');
 
       self.tracer = new node_tracers.RESTkinTracer('http://localhost:22222',
