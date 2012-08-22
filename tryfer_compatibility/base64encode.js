4455446f8b51169f1c64c4625b4db7dd36c94bdc
cyli <ying.li@rackspace.com>
2012-08-22 12:57:18 -0700

    Add endpoints to tryfer compatibility code, fix bug where ipv4->int was being calculated backwards

--- tryfer_compatibility/base64encode.js
+++ tryfer_compatibility/base64encode.js
@@ -10,12 +10,16 @@ var trace = require('..').trace;
 
 var sample_trace = new trace.Trace('trace', {
   traceId: 100, spanId: 10, parentSpanId: 5});
+var endpoint = new trace.Endpoint('1.2.3.4', 8080, 'myservice');
 var sample_annotations = [new trace.Annotation.timestamp('mytime', 1),
                           new trace.Annotation.string('mystring', 'value')];
+sample_annotations.forEach(function(ann) {
+  ann.endpoint = endpoint;
+});
 
-var expected_base64 = "CgABAAAAAAAAAGQLAAMAAAAFdHJhY2UKAAQAAAAAAAAACgoABQAAAAAAAAAFDwAGDAAAAAEKAAEAAAAAAAAAAQsAAgAAAAZteXRpbWUADwAIDAAAAAELAAEAAAAIbXlzdHJpbmcLAAIAAAAFdmFsdWUIAAMAAAAGAAA=";
+var expected_base64 = "CgABAAAAAAAAAGQLAAMAAAAFdHJhY2UKAAQAAAAAAAAACgoABQAAAAAAAAAFDwAGDAAAAAEKAAEAAAAAAAAAAQsAAgAAAAZteXRpbWUMAAMIAAEBAgMEBgACH5ALAAMAAAAJbXlzZXJ2aWNlAAAPAAgMAAAAAQsAAQAAAAhteXN0cmluZwsAAgAAAAV2YWx1ZQgAAwAAAAYAAA==";
 
-var expected_json = '{"annotations": [{"type": "timestamp", "value": 1, "key": "mytime"}, {"type": "string", "value": "value", "key": "mystring"}], "parent_span_id": "0000000000000005", "trace_id": "0000000000000064", "name": "trace", "span_id": "000000000000000a"}';
+var expected_json = '[{"annotations": [{"host": {"service_name": "myservice", "ipv4": "1.2.3.4", "port": 8080}, "type": "timestamp", "value": 1, "key": "mytime"}, {"host": {"service_name": "myservice", "ipv4": "1.2.3.4", "port": 8080}, "type": "string", "value": "value", "key": "mystring"}], "parent_span_id": "0000000000000005", "trace_id": "0000000000000064", "name": "trace", "span_id": "000000000000000a"}]';
 
 formatters.formatForZipkin(sample_trace, sample_annotations,
   function(error, value) {
--- tryfer_compatibility/base64encode.py
+++ tryfer_compatibility/base64encode.py
@@ -4,11 +4,14 @@ set of annotations.
 """
 
 from tryfer import formatters
-from tryfer.trace import Trace, Annotation
+from tryfer.trace import Trace, Endpoint, Annotation
 
 trace = Trace('trace', trace_id=100, span_id=10, parent_span_id=5)
+endpoint = Endpoint('1.2.3.4', 8080, 'myservice')
 annotations = [Annotation.timestamp('mytime', 1),
                Annotation.string('mystring', 'value')]
+for annotation in annotations:
+    annotation.endpoint = endpoint
 
 print formatters.base64_thrift_formatter(trace, annotations)
 print formatters.json_formatter(trace, annotations)
