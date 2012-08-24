ec41905d7d2bef1e0fed76d117723f6e0333f168
cyli <ying.li@rackspace.com>
2012-08-24 15:14:08 -0700

    Change parameter name of Trace.fromRequest, because it could be otherwise named.  Fix, comment, and license up examples

--- examples/tracing_client.js
+++ examples/tracing_client.js
@@ -1,9 +1,23 @@
+// Copyright 2012 Rackspace Hosting, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 var request = require('request');
 
 var trace = require('..').trace;
 var tracers = require('..').tracers;
 
-var method, uri, t, tget;
+var method, uri, t;
 
 if (process.argv.length < 4) {
   console.log("Usage: " +
@@ -14,22 +28,34 @@ if (process.argv.length < 4) {
 method = process.argv[2];
 uri = process.argv[3];
 
-t = new trace.Trace('fetch_a_thing');
+// DebugTracer prints traces to stdout
 tracers.pushTracer(new tracers.DebugTracer(process.stdout));
 
-tget = t.child('GET');
-tget.record(trace.Annotation.uri(uri));
-tget.record(trace.Annotation.clientSend());
+// The trace used for http requests, by Tryfer convention, should be named by
+// the request method
+t = new trace.Trace('GET');
+
+// record the URI and send out a CLIENT_SEND annotation before actually making
+// the request
+t.record(trace.Annotation.uri(uri));
+t.record(trace.Annotation.clientSend());
+
+// Make the request after the CLIENT_SEND annotation has been recorded
 request(
-  {method: method, uri: uri, headers: tget.toHeaders()},
+  {method: method, uri: uri, headers: t.toHeaders()},
   function (error, response, body) {
-    tget.record(trace.Annotation.clientRecv());
+
+    // by Tryfer convention, and by what finagle does, a CLIENT_RECV annotation
+    // should be made as soon as a response is returned
+    t.record(trace.Annotation.clientRecv());
+
+    // Other annotations based on the response can also be made
     if (error !== undefined && error !== null) {
-      tget.record(trace.Annotation.string(error.toString()));
+      t.record(trace.Annotation.string(error.toString()));
     } else {
-      tget.record(trace.Annotation.string('status_code',
+      t.record(trace.Annotation.string('status_code',
                                           response.statusCode.toString()));
-      tget.record(trace.Annotation.string('headers', JSON.stringify(response.headers)));
-      tget.record(trace.Annotation.string('body', body));
+      t.record(trace.Annotation.string('headers', JSON.stringify(response.headers)));
+      t.record(trace.Annotation.string('body', body));
     }
   });
--- examples/tracing_server.js
+++ examples/tracing_server.js
@@ -1,21 +1,45 @@
+// Copyright 2012 Rackspace Hosting, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 var express = require('express');
 
 var trace = require('..').trace;
 var tracers = require('..').tracers;
 
+// DebugTracer prints traces to stdout
 tracers.pushTracer(new tracers.DebugTracer(process.stdout));
 
+// Create an express server
 var app = express();
 
 app.get('/', function(request, response) {
-  console.log(request.headers);
+  // Create a trace from the request headers. The trace used for http
+  // requests, by Tryfer convention, should be named by the request method
   var t = trace.Trace.fromRequest(request.method, request.headers);
+
+  // Record the server receive annotation
   t.record(trace.Annotation.serverRecv());
+
+  // Other annotations can also be recorded
   t.record(trace.Annotation.string('request_headers',
                                    JSON.stringify(request.headers)));
   response.statusCode = 200;
   response.write('this is the body');
   response.end();
+
+  // By Tryfer convention, and by what finagle does, a SERVER_SEND annotation
+  // should be made as soon as a response is sent
   t.record(trace.Annotation.serverSend());
 });
 
